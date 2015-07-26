57cf43dffc92ebe225218ab4fcb406c3c48b7c87
Roberto Reale <roberto.reale82@gmail.com>
2015-07-26 14:44:57 +0200

    Review run_analysis.R.

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,7 @@
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation
 ##    for each measurement. 
-## 3. Uses desciptive activity names to name the activities in the data set.
+## 3. Uses descriptive activity names to name the activities in the data set.
 ## 4. Appropriately labels the data set with descriptive variable names.
 ## 5. From the data set in step 4, it creates a second, independent tidy
 ##    data set with the average of each variable for each activity
@@ -80,8 +80,8 @@ features[,2] <- as.character(features[,2])
 # select "wanted" features
 featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
 featuresWanted.names <- features[featuresWanted,2]
-featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
-featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names <- gsub('-std', 'Std', featuresWanted.names)
 featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
 # extract "wanted" columns from data set
